51cdca1f558b539e4bf68611abf228e508437341
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-08-05 10:00:10 -0300

    add candidate and dependencies

---
+++ src/main/java/com/pedromelobitencourt/job_vacancy_management/modules/candidate/CandidateEntity.java
@@ -0,0 +1,26 @@
+package com.pedromelobitencourt.job_vacancy_management.modules.candidate;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+@Data
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 5, max = 100)
+    private String password;
+    private String description;
+    private String curriculum;
+}
---
+++ src/main/java/com/pedromelobitencourt/job_vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,23 @@
+package com.pedromelobitencourt.job_vacancy_management.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedromelobitencourt.job_vacancy_management.modules.candidate.CandidateEntity;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/candidates")
+public class CandidateController {
+    @PostMapping("/")
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato:");
+        System.out.println(candidateEntity.getName());
+        System.out.println(candidateEntity.getEmail());
+    }
+}
