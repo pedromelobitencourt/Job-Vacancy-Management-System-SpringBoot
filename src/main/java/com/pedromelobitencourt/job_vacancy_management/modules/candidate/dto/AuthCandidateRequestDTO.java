c5d15f1fe8b3505528b621cf1aea63c58b152664
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-08-24 23:15:11 -0300

    start adding candidate security

---
+++ src/main/java/com/pedromelobitencourt/job_vacancy_management/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,5 @@
+package com.pedromelobitencourt.job_vacancy_management.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+    
+}
---
+++ src/main/java/com/pedromelobitencourt/job_vacancy_management/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,49 @@
+package com.pedromelobitencourt.job_vacancy_management.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.pedromelobitencourt.job_vacancy_management.modules.candidate.CandidateRepository;
+import com.pedromelobitencourt.job_vacancy_management.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.pedromelobitencourt.job_vacancy_management.modules.candidate.dto.AuthCandidateResponseDTO;
+
+@Service
+public class AuthCandidateUseCase {
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+        var candidate = candidateRepository.findByUsername(authCandidateRequestDTO.username())
+            .orElseThrow(() -> { throw new UsernameNotFoundException("Username/password incorrect"); });
+        var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if(!passwordMatches) throw new AuthenticationException();
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create()
+            .withIssuer("javacancy")
+            .withSubject(candidate.getId().toString())
+            .withClaim("roles", Arrays.asList("candidate"))
+            .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+            .sign(algorithm);
+        var authCandidateResponseDTO = AuthCandidateResponseDTO.builder().access_token(token).build();
+        return authCandidateResponseDTO;
+    }
+}
